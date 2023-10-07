441a588116352695e83a70482c77e6c8af873d85
Lubyansky <noobsa87@gmail.com>
2023-10-07 17:21:32 +0500

    Create and display contribution types

--- script.js
+++ script.js
@@ -4,10 +4,33 @@ new Vue({
     el: '#ContributionGraph',
     data(){
         return{
+            contributionTypes: [
+                {
+                  description: "No contributions",
+                  color: "#ededed"
+                },
+                {
+                  description: "1-9 contributions",
+                  color: "#acd5f2"
+                },
+                {
+                  description: "10-19 contributions",
+                  color: "#7fa8c9"
+                },
+                {
+                  description: "20-29 contributions",
+                  color: "#527ba0"
+                },
+                {
+                  description: "30+ contributions",
+                  color: "#254e77"
+                }
+            ],
             contributionList: {
-              url: 'https://dpg.gg/test/calendar.json',
-              data: {}
-            }
+                url: 'https://dpg.gg/test/calendar.json',
+                data: {}
+            },
+            //cells: []
         }
     },
     async mounted() {
