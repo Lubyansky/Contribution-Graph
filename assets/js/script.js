dd9919e007eb42e86a97965c17b7c6cddc69e8f8
Lubyansky <noobsa87@gmail.com>
2023-10-07 23:26:08 +0500

    Add styles and cell component

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,17 @@
 import Vue from 'https://cdn.jsdelivr.net/npm/vue@2.7.8/dist/vue.esm.browser.js'
 
 new Vue({
-    el: '#ContributionGraph',
+    el: '#contribution-graph',
     data(){
         return{
             contributionTypes: [
                 {
                   description: "No contributions",
-                  color: "#ededed"
+                  color: "#ededed",
                 },
                 {
                   description: "1-9 contributions",
-                  color: "#acd5f2"
+                  color: "#acd5f2",
                 },
                 {
                   description: "10-19 contributions",
@@ -34,7 +34,9 @@ new Vue({
                 size: 357,
                 data: new Object()
             },
-            currentDate: Date.now()
+            currentDate: Date.now(),
+            isLoading: true,
+            selectedCell: -1
         }
     },
 
@@ -57,7 +59,7 @@ new Vue({
             let month = date.getMonth();
             let day = date.getDate();
 
-            //month = (month == 0) ? '12' : month
+            month = (month == 0) ? '12' : month
             month = (month < 10) ? '0' + month : month;
             day = (day < 10) ? '0' + day : day;
             
@@ -72,7 +74,7 @@ new Vue({
 
             for(var value in arr){
                 let dateValue = new Date(this.currentDate - dayMilliseconds * value)
-                this.cells.data[this.formateDate(dateValue)] = {contributionCount: 0}
+                this.cells.data[this.formateDate(dateValue)] = {contributionCount: 0, color: '#000000'}
             }
         },
 
@@ -83,17 +85,36 @@ new Vue({
             
             for (var date in this.cells.data) {
                 if(contributionList[date]){
-                    resultDictionary[date] = {contributionCount: contributionList[date]}
+                    resultDictionary[date] = {contributionCount: contributionList[date], color: '#000000'}
                 }
                 else {
                     resultDictionary[date] = {contributionCount: 0}
                 }
+
+                if(resultDictionary[date].contributionCount == 0){
+                    resultDictionary[date].color = this.contributionTypes[0].color
+                }
+                if(resultDictionary[date].contributionCount >= 1 && resultDictionary[date].contributionCount <= 9){
+                    resultDictionary[date].color = this.contributionTypes[1].color
+                }
+                if(resultDictionary[date].contributionCount >= 10 && resultDictionary[date].contributionCount <= 19){
+                    resultDictionary[date].color = this.contributionTypes[2].color
+                }
+                if(resultDictionary[date].contributionCount >= 20 && resultDictionary[date].contributionCount <= 29){
+                    resultDictionary[date].color = this.contributionTypes[3].color
+                }
+                if(resultDictionary[date].contributionCount >= 30){
+                    resultDictionary[date].color = this.contributionTypes[4].color
+                }
             }
 
             this.cells.data = resultDictionary
+            console.log(this.cells.data)
+        },
+
+        selectCell(index){
+            this.selectedCell = index
         }
-
-
     },
 
 
@@ -106,6 +127,40 @@ new Vue({
         await this.loadContributionData();
 
         await this.fillCells()
+        this.isLoading = false
     } 
 
 })
+
+Vue.component('contribution-cell',{
+    props:['cell', 'index', 'selected-cell'],
+    emits: ['select-cell'],
+    template: '#contribution-cell',
+    data(){
+        return{
+            isPressed: false
+        }
+    },
+    methods: {
+        getContributionsCount(){
+            if(this.cell.description){
+                return this.cell.description
+            }
+            return (this.cell.contributionCount > 0 ? this.cell.contributionCount : `No`)  + ` contributions`;
+        },
+        /*Click(){
+            if(this.isPressed){
+                this.isPressed = false
+            }
+            else {
+                this.isPressed = true
+            }
+
+            this.$emit('selectCell', this.index)
+
+            if(this.selectedCell != this.index){
+                this.isPressed = false
+            }
+        }*/
+    }
+});
